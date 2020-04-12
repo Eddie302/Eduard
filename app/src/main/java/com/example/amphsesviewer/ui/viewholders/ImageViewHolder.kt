24fc9ad80ea4c3d65e3abb94b78a3a5fffb5e6f8
Eddie302 <akma3750@gmail.com>
2020-04-12 16:39:34 +0700

    add progressbar to imageListItem

--- app/src/main/java/com/example/amphsesviewer/ui/viewholders/ImageViewHolder.kt
+++ app/src/main/java/com/example/amphsesviewer/ui/viewholders/ImageViewHolder.kt
@@ -2,12 +2,19 @@ package com.example.amphsesviewer.ui.viewholders
 
 import android.view.View
 import androidx.appcompat.widget.AppCompatImageView
+import androidx.core.widget.ContentLoadingProgressBar
 import androidx.recyclerview.widget.RecyclerView
 import com.example.amphsesviewer.R
 import com.example.amphsesviewer.domain.model.ImageData
 
 class ImageViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
     fun bind(imageData: ImageData) {
+        val pb = view.findViewById<ContentLoadingProgressBar>(R.id.pb)
+        if (imageData.bitmap != null) {
+            pb.hide()
+        } else {
+            pb.show()
+        }
         view.findViewById<AppCompatImageView>(R.id.image).setImageBitmap(imageData.bitmap)
     }
 }
